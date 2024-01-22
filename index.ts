c6958f4a6ce9952f2f156d490680a1399f9dec2f
Gabriel Cozma <me@gabs.eu.org>
2024-01-22 23:19:59 +0200

    cleanup: Formatting and actually using SCSS

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 // https://vitepress.dev/guide/custom-theme
-import Layout from './Layout.vue'
-import DefaultTheme from 'vitepress/theme'
-import type { Theme } from 'vitepress'
-import './styles/index.scss'
-import 'inter-ui/inter.css';
-import 'inter-ui/inter-variable.css';
-import 'inter-ui/inter-display.css';
+import Layout from "./Layout.vue";
+import DefaultTheme from "vitepress/theme";
+import type { Theme } from "vitepress";
+import "./styles/index.scss";
+import "inter-ui/inter.css";
+import "inter-ui/inter-variable.css";
+import "inter-ui/inter-display.css";
 import "@fontsource/source-code-pro/500.css";
 import "@fontsource/noto-emoji/700.css";
 import "@fontsource/noto-color-emoji/400.css";
@@ -13,5 +13,4 @@ import "@fontsource/noto-color-emoji/400.css";
 export default {
   extends: DefaultTheme,
   Layout: Layout,
-
-} satisfies Theme
+} satisfies Theme;
