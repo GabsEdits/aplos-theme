d7fd1db95e4d3af74ec8997c657c9e51b99002a5
Gabriel Cozma <me@gabs.eu.org>
2024-02-24 21:58:46 +0200

    fix: Oh wait is Vue

--- components/BlogFooter.vue
+++ components/BlogFooter.vue
@@ -5,11 +5,7 @@
         <p>
             If you have any questions or comments about this post, please feel free to leave a comment below.
         </p>
-        <script src="https://giscus.app/client.js" :data-repo="theme.blog.data.repo" :data-repo-id="theme.blog.data.repo"
-            data-category="Blog Comments" :data-category-id="theme.blog.data.categoryid" data-mapping="url" data-strict="0"
-            data-reactions-enabled="1" data-emit-metadata="0" data-input-position="bottom"
-            data-theme="preferred_color_scheme" data-lang="en" crossorigin="anonymous" async>
-        </script>
+        <div id="giscusContainer"></div>
         <a href="/blog/">
             <span>← Go back to the blog's home</span>
         </a>
@@ -18,8 +14,31 @@
 
 <script setup lang="ts">
 import { useData } from 'vitepress';
+import { onMounted } from 'vue';
 
 const { theme } = useData();
+
+onMounted(() => {
+  // Load GitHub Comments script dynamically
+  const script = document.createElement("script");
+  script.src = "https://giscus.app/client.js";
+  script.async = true;
+script.setAttribute("data-repo", theme.value.blog.data.repo);
+  script.setAttribute("data-repo-id", theme.value.blog.data.repoid);
+  script.setAttribute("data-category", "Blog Comments");
+  script.setAttribute("data-category-id", theme.value.blog.data.categoryid);
+  script.setAttribute("data-mapping", "url");
+  script.setAttribute("data-strict", "0");
+  script.setAttribute("data-reactions-enabled", "1");
+  script.setAttribute("data-emit-metadata", "0");
+  script.setAttribute("data-input-position", "bottom");
+  script.setAttribute("data-theme", "preferred_color_scheme");
+  script.setAttribute("data-lang", "en");
+  script.setAttribute("crossorigin", "anonymous");
+
+  // Append the script to the document
+document.getElementById("giscusContainer")?.appendChild(script);
+});
 </script>
 
 <style lang="scss">
