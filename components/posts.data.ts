fb67a7fc1377e6e2e91f7f84bdf1f24ac5ac7846
Gabriel Cozma <me@gabs.eu.org>
2024-04-06 13:16:19 +0300

    cleanup: Format Posts Data, fix ToC

--- components/posts.data.ts
+++ components/posts.data.ts
@@ -10,25 +10,25 @@ declare const data: Post[];
 export { data };
 
 export default createContentLoader("blog/posts/*.md", {
-    excerpt: true,
-    transform(raw): Post[] {
-        return raw
-            .map(({ frontmatter }) => ({
-                title: frontmatter.title,
-                description: frontmatter.shortDescription,
-                tags: frontmatter.tags,
-                date: formatDate(frontmatter.date),
-            }))
-            .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
-    },
+  excerpt: true,
+  transform(raw): Post[] {
+    return raw
+      .map(({ frontmatter }) => ({
+        title: frontmatter.title,
+        description: frontmatter.shortDescription,
+        tags: frontmatter.tags,
+        date: formatDate(frontmatter.date),
+      }))
+      .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+  },
 });
 
 function formatDate(raw: string): string {
-    const date = new Date(raw);
-    date.setUTCHours(12);
-    return date.toLocaleDateString("en-GB", {
-        year: "numeric",
-        month: "long",
-        day: "2-digit",
-    });
+  const date = new Date(raw);
+  date.setUTCHours(12);
+  return date.toLocaleDateString("en-GB", {
+    year: "numeric",
+    month: "long",
+    day: "2-digit",
+  });
 }
